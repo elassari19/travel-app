9cfdb0175107d0fbac18fe34b5437b74c7a6fe5c
mac <mac@MacBook-Pro-de-mac.local>
2023-03-01 02:07:03 +0100

    scaffold singin controller file

--- micro-auth/src/api/index.ts
+++ micro-auth/src/api/index.ts
@@ -1,17 +1,18 @@
 import express from 'express';
 import insertUser from './signup';
 import { Signup } from './signup/_.type';
-import { validator } from '../middlewares';
+import { authenticated, validator } from '../middlewares';
 import verifyUser from './verify';
 import { ReSendEamil } from './reSendEmail/_.type';
 import sendEmail from './reSendEmail';
 import { SignIn } from './signIn/_.type';
+import signInUser from './signIn';
 
 const router = express.Router();
 
 router.post('/signup', validator(Signup), insertUser)
 router.post('/sendemail', validator(ReSendEamil), sendEmail)
 router.get('/verify', verifyUser)
-router.get('/signin', validator(SignIn), verifyUser)
+router.post('/signin', validator(SignIn), authenticated(false), signInUser)
 
 export default router;
---
+++ micro-auth/src/api/signIn/index.ts
@@ -0,0 +1,57 @@
+import { NextFunction, Request, Response } from "express"
+import { WithId } from "mongodb"
+import { hash, jwt } from "../../utilities"
+import { Users } from "../../modules"
+import { ISignIn } from "./_.type"
+import cookie from "cookie";
+
+export type withid = WithId<ISignIn> 
+
+const signInUser = async (req: Request<ISignIn>, res: Response, next: NextFunction) => {
+  try {
+
+    // get query
+    const { email, password } = req.body;
+
+    // find user by id
+    const result = await Users.findOne({ email }, { _id: 1, email: 1, password: 1, verified: 1 })
+
+    // id not valid
+    if (!result?._id) return res.status(404).send('email not found')
+
+    // id not valid
+    if (result.verified !== true) return res.status(406).send('account not verified')
+
+    // password not valid
+    if (result.password !== hash(password as string)) return res.status(406).send('password not valid')
+
+    // sign in succeeded
+    // @ts-ignore
+    const token = jwt.sign({ user_id: result._id, email: result.email })
+    await Users.updateOne(
+      { email },
+      { $set: { token }, },
+      { new: true }
+    ).then((response) => {
+      console.log('req', response)
+      res.setHeader(
+        "Set-Cookie",
+        cookie.serialize("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV !== "development",
+          maxAge: 60 * 60 * 60,
+          sameSite: "strict",
+          path: "/",
+        })
+      )
+      .status(200)
+      .send(response);
+    })
+
+  } catch (error) {
+    console.log(error)
+    next(error)
+  }
+}
+
+export default signInUser
